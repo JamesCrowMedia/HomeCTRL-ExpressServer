512d0579c8f928f101fa9cfc88ab0a8873dbb621
James Crow <jamescrowvideo@gmail.com>
2017-11-25 20:12:19 -0600

    Added cmd strings

--- index.js
+++ index.js
@@ -1,5 +1,51 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const cmd = require('node-cmd');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+const serviceAccount = require('./secret/service-account.json');
+const firebase = require("firebase-admin");
+
+let currentState = {}
+const rfSwitch = '../rfoutlet/codesend'
+
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://homectrl-jamescrow.firebaseio.com"
+});
+
+//firebase.database.enableLogging(true);
+
+var ref = firebase.app().database().ref('rfLights');
+
+
+ref.once('value')
+ .then(function (snap) {
+     currentState = snap.val();
+ console.log('currentState', currentState);
+ });
+
+ ref.on('child_changed',function(childsnapshot,prevchildname){
+    console.log(prevchildname);
+    let change = childsnapshot.val();
+
+    if(change.state === true || change.state === 'true'){
+        console.log(change.desc + ': on - ' + change.on)
+        cmd.run(rfSwitch + ' ' + change.on);
+    } else {
+        console.log(change.desc + ': off - ' + change.off)
+        cmd.run(rfSwitch + ' ' + change.off);        
+    }
+
+     
+    ref.once('value')
+    .then(function (snap) {
+        
+    //console.log('snap.val()', snap.val());
+    });
+  }) ;
+
+app.get('/', (req, res) => res.send(currentState))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+
